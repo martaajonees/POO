7732bf9b6766fe6d2d75077605e827eb3352eb4e
martaajonees <100365874+martaajonees@users.noreply.github.com>
2023-05-12 17:47:42 +0200

    inicial

--- P3/pedido-articulo.d
+++
@@ -1,3 +0,0 @@
-pedido-articulo.o pedido-articulo.d: pedido-articulo.cpp pedido-articulo.hpp pedido.hpp \
-  tarjeta.hpp ../P1/cadena.hpp ../P1/fecha.hpp usuario-pedido.hpp \
-  articulo.hpp usuario.hpp
--- P3/pedido.d
+++
@@ -1,3 +0,0 @@
-pedido.o pedido.d: pedido.cpp pedido.hpp tarjeta.hpp ../P1/cadena.hpp \
-  ../P1/fecha.hpp usuario-pedido.hpp articulo.hpp usuario.hpp \
-  pedido-articulo.hpp
